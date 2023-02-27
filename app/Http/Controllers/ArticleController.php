330c09fa67b759ff35f5c239c69b4ef20611f9ac
jose.regalado <jose.regalado@irapuato.gob.mx>
2023-02-27 15:15:47 -0600

    Avance curso

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -82,8 +82,9 @@ class ArticleController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Article $article)
     {
-        //
+        $article->delete();
+        return redirect()->route('articles.index')->with('success', 'Articulo eliminado');
     }
 }
--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -8,11 +8,41 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                    @if(session('success'))
+
+                    <div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md"
+                        role="alert">
+                        <div class="flex">
+                            <div class="py-1"><svg class="fill-current h-6 w-6 text-teal-500 mr-4"
+                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path
+                                        d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
+                                </svg></div>
+                            <div>
+                                <p class="font-bold">Exito</p>
+                                <p class="text-sm">{{session('success')}}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    @endif
+
+
                     <div class="container">
                         @foreach ($articles as $article)
                         <div>
                             <p>{{ $article->title}}</p>
                             <a href="{{route('articles.show',$article)}}">Show</a>
+                        </div>
+                        <div>
+                            <form action="{{ route('articles.destroy',$article) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit"
+                                    class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                                    Eliminar
+                                </button>
+                            </form>
                         </div>
                         @endforeach
                         {{ $articles->links()}}
